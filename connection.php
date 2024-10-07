666e7752aa3a52f0042bedb43095e7a7a5109063
Maneesha Geethanga <118714345+ManeeshaGee@users.noreply.github.com>
2024-10-07 19:16:05 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,31 @@
+<?php
+
+Class Database{
+ 
+	private $server = "mysql:host=localhost;dbname=newdb";
+	private $username = "root";
+	private $password = "1234";
+	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+	protected $conn;
+ 	
+	public function open(){
+ 		try{
+ 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+			echo "No problem with database connection";
+ 			return $this->conn;
+ 		}
+ 		catch (PDOException $e){
+ 			echo "There is some problem in connection: " . $e->getMessage();
+ 		}
+ 
+    }
+ 
+	public function close(){
+   		$this->conn = null;
+ 	}
+ 
+    }
+
+    $pdo = new Database();
+    //$pdo->open();
+?>
