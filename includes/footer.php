94b4d8cc78264e682a0590462ae38f6148949a45
Asela <43272744+zaselalk@users.noreply.github.com>
2024-10-19 16:01:55 +0530

    Update footer and header sections

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,53 @@
 <footer id="footer" class="footer dark-background">
     <div class="container">
-        <h5 class="sitename">Alumni | Faculty of Technology - University of Ruhuna</h5>
-        <div class="social-links d-flex justify-content-center">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-skype"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
+        <div class="row">
+            <div class="col-md-4">
+                <h5 class="text-start text-bold">Quick Links</h5>
+                <ul class="list-unstyled text-start">
+                    <li><a href="#" class="footer-link">Faculty of Agriculture</a></li>
+                    <li><a href="#" class="footer-link">Faculty of Engineering</a></li>
+                    <li><a href="#" class="footer-link">Faculty of Humanities and Social.Sci.</a></li>
+                    <li><a href="#" class="footer-link">Faculty of Management and Finance</a></li>
+                    <li><a href="#" class="footer-link">Faculty of Medicine</a></li>
+                    <li><a href="#" class="footer-link">Faculty of Science</a></li>
+                    <li><a href="#" class="footer-link">Faculty of Fisheries and Marine Sci.</a></li>
+                    <li><a href="#" class="footer-link">Faculty of Graduate Studies</a></li>
+                    <li><a href="#" class="footer-link">Faculty of Allied Health Sciences</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-12 ">
+                        <h5 class="text-start text-bold">Contact Details</h5>
+                        <p class="text-start">Contact us: 0112 903 838</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-4">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-12 ">
+                        <h5 class="text-start text-bold">Location</h5>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15873.595498199173!2d80.5773903!3d5.9396338!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae13904aa7a00d9%3A0xba37fdf4d892a52b!2sFaculty%20of%20Technology!5e0!3m2!1sen!2slk!4v1729333373098!5m2!1sen!2slk"
+                            width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h5 class="sitename">Alumni | Faculty of Technology - University of Ruhuna</h5>
+                <div class="social-links d-flex justify-content-center">
+                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
         </div>
 
     </div>
--- includes/header.php
+++ includes/header.php
@@ -5,15 +5,15 @@
         </a>
         <nav id="navmenu" class="navmenu">
             <ul>
-                <li><a href="index.php#hero">Home</a></li>
-                <li><a href="index.php#about">About</a></li>
-                <li><a href="index.php#gallery">Gallery</a></li>
-                <li><a href="index.php#team">Members</a></li>
+                <li><a href="index.php#hero">HOME</a></li>
+                <li><a href="index.php#about">ABOUT</a></li>
+                <li><a href="index.php#gallery">GALLERY</a></li>
+                <li><a href="index.php#team">OFFICE BEARERS</a></li>
+                <li><a href="index.php#contact">CONTACT</a></li>
                 <li>
                     <a href="news.php" <a href="news.php"
-                        class="<?php echo basename($_SERVER['PHP_SELF']) == 'news.php' || basename($_SERVER['PHP_SELF']) == 'news-details.php' ? 'active' : ''; ?>">News</a>
+                        class="<?php echo basename($_SERVER['PHP_SELF']) == 'news.php' || basename($_SERVER['PHP_SELF']) == 'news-details.php' ? 'active' : ''; ?>">NEWS</a>
                 </li>
-                <li><a href="index.php#contact">Contact</a></li>
             </ul>
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
